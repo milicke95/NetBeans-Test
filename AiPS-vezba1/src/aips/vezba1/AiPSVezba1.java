97db039bbfce8436a757119df6ddd153da26b370
student <student@521-10>
2017-10-20 13:13:52 +0200

    Initialize project

---
+++ AiPS-vezba1/src/aips/vezba1/AiPSVezba1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aips.vezba1;
+
+/**
+ *
+ * @author student
+ */
+public class AiPSVezba1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
